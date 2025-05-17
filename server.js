87e825d59c0b91ec09049fcc8fe5ef2615cab5a4
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-05-17 19:38:36 -0300

    highlight endpoint

--- server.js
+++ server.js
@@ -12,17 +12,29 @@ server.use('/assets', express.static(path.join(__dirname, 'assets')))
 
 server.use(middlewares)
 
-// Endpoints personalizados
+// Endpoint personalizado: /promo
 server.get('/promo', (req, res) => {
   const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8'))
   const promo = db.games.filter((game) => game.prices.discount != null)
   res.json(promo)
 })
 
+// Endpoint personalizado: /comingsoon
 server.get('/comingsoon', (req, res) => {
   const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8'))
   const comingSoon = db.games.filter((game) => game.prices.current == null)
   res.json(comingSoon)
+})
+
+// Novo endpoint personalizado: /highlight (retorna um game aleatório)
+server.get('/highlight', (req, res) => {
+  const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8'))
+  const allGames = db.games
+  if (allGames.length === 0) {
+    return res.status(404).json({ error: 'No games found' })
+  }
+  const randomIndex = Math.floor(Math.random() * allGames.length)
+  res.json(allGames[randomIndex])
 })
 
 // Rota padrão do JSON Server
